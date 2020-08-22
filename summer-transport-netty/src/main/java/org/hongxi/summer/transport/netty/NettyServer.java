2c2ea709a4b25f70312e6cd3ca21c0dea7e65a52
javahongxi <javahongxi@qq.com>
2020-08-22 22:56:34 +0800

    core

--- summer-transport-netty/src/main/java/org/hongxi/summer/transport/netty/NettyServer.java
+++ summer-transport-netty/src/main/java/org/hongxi/summer/transport/netty/NettyServer.java
@@ -111,8 +111,7 @@ public class NettyServer extends AbstractServer {
                         pipeline.addLast("channel_manage", channelManage);
                         pipeline.addLast("decoder", new NettyDecoder(codec, NettyServer.this, maxContentLength));
                         pipeline.addLast("encoder", new NettyEncoder());
-                        NettyChannelHandler handler = new NettyChannelHandler(NettyServer.this, messageHandler, threadPoolExecutor);
-                        pipeline.addLast("handler", handler);
+                        pipeline.addLast("handler", new NettyChannelHandler(NettyServer.this, messageHandler, threadPoolExecutor));
                     }
                 });
         serverBootstrap.childOption(ChannelOption.TCP_NODELAY, true);
