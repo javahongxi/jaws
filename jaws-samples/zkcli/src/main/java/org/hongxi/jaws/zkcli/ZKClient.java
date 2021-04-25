145593074afbc007b194f41e1a935fd42db13682
javahongxi <javahongxi@qq.com>
2021-04-25 16:59:27 +0800

    优化

---
+++ jaws-samples/zkcli/src/main/java/org/hongxi/jaws/zkcli/ZKClient.java
@@ -0,0 +1,15 @@
+package org.hongxi.jaws.zkcli;
+
+import org.apache.zookeeper.ZooKeeperMain;
+
+/**
+ * Created by shenhongxi on 2020/9/17.
+ */
+public class ZKClient {
+
+    public static void main(String[] args) throws Exception {
+        ZooKeeperMain.main(args);
+        // 列出根节点下的所有子节点：ls /
+        // 列出某节点下的所有子节点：ls /dubbo
+    }
+}
