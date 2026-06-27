54a4b4204686d87cb61872991c9c10999d03a3a7
javahongxi <javahongxi@qq.com>
2026-06-28 06:55:56 +0800

    remove lombok

--- jaws-samples/jaws-sample-api/src/main/java/org/hongxi/jaws/sample/api/model/Contacts.java
+++ jaws-samples/jaws-sample-api/src/main/java/org/hongxi/jaws/sample/api/model/Contacts.java
@@ -1,19 +1,13 @@
 package org.hongxi.jaws.sample.api.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by shenhongxi on 2021/4/26.
  */
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Contacts implements Serializable {
     @Serial
     private static final long serialVersionUID = 9095342848908217853L;
@@ -25,4 +19,64 @@ public class Contacts implements Serializable {
     private List<Phone> phones;
 
     private List<String> addresses;
+
+    public Contacts() {
+    }
+
+    public Contacts(Long id, User user, List<Phone> phones, List<String> addresses) {
+        this.id = id;
+        this.user = user;
+        this.phones = phones;
+        this.addresses = addresses;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public List<Phone> getPhones() {
+        return phones;
+    }
+
+    public void setPhones(List<Phone> phones) {
+        this.phones = phones;
+    }
+
+    public List<String> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<String> addresses) {
+        this.addresses = addresses;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contacts contacts = (Contacts) o;
+        return Objects.equals(id, contacts.id) && Objects.equals(user, contacts.user) && Objects.equals(phones, contacts.phones) && Objects.equals(addresses, contacts.addresses);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, user, phones, addresses);
+    }
+
+    @Override
+    public String toString() {
+        return "Contacts{id=" + id + ", user=" + user + ", phones=" + phones + ", addresses=" + addresses + "}";
+    }
 }
--- jaws-samples/jaws-sample-api/src/main/java/org/hongxi/jaws/sample/api/model/User.java
+++ jaws-samples/jaws-sample-api/src/main/java/org/hongxi/jaws/sample/api/model/User.java
@@ -1,18 +1,12 @@
 package org.hongxi.jaws.sample.api.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  * Created by shenhongxi on 2021/4/25.
  */
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class User implements Serializable {
 
     @Serial
@@ -21,4 +15,46 @@ public class User implements Serializable {
     private String name;
 
     private int age;
+
+    public User() {
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return age == user.age && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public String toString() {
+        return "User{name='" + name + "', age=" + age + "}";
+    }
 }
