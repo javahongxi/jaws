3fa371e94cad83a2d681ae22019b3a58377f449d
javahongxi <javahongxi@qq.com>
2026-07-19 14:04:50 +0800

    router

--- jaws-core/src/main/java/org/hongxi/jaws/cluster/router/GroupRouter.java
+++ jaws-core/src/main/java/org/hongxi/jaws/cluster/router/GroupRouter.java
@@ -1,15 +1,14 @@
 package org.hongxi.jaws.cluster.router;
 
 import org.hongxi.jaws.cluster.AbstractRouter;
-import org.hongxi.jaws.common.URLParamType;
 import org.hongxi.jaws.common.extension.SpiMeta;
 import org.hongxi.jaws.rpc.URL;
 
 /**
  * Routes requests to providers in a specific group.
  * <p>
- * When consumer url has {@code routeGroup} parameter set, only provider urls
- * whose group matches will be kept. If not set, all providers pass through.
+ * When consumer url has a group set, only provider urls whose group matches
+ * will be kept. If not set, all providers pass through.
  * <p>
  * Created by shenhongxi on 2025/7/19.
  */
@@ -18,10 +17,10 @@ public class GroupRouter extends AbstractRouter {
 
     @Override
     protected boolean match(URL providerUrl, URL consumerUrl) {
-        String routeGroup = consumerUrl.getParameter(URLParamType.routeGroup.getName(), URLParamType.routeGroup.value());
-        if (routeGroup == null || routeGroup.isEmpty()) {
+        String group = consumerUrl.getGroup();
+        if (group == null || group.isEmpty()) {
             return true;
         }
-        return routeGroup.equals(providerUrl.getGroup());
+        return group.equals(providerUrl.getGroup());
     }
 }
